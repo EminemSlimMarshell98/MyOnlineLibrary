5695da561c4467f267a47c029bd129fc77bfaaa6
EminemSlimMarshell98 <39140484+EminemSlimMarshell98@users.noreply.github.com>
2018-05-30 19:05:56 -0400

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,48 @@
+<?php
+*//Books*
+$catalog[101] = [
+    "title" => "Percy Jackson & the Olympians",
+    "img" => "media/img/Book4.jpg",
+   "genre" => "Fiction",
+   "format" => "Paperback",
+   "year" => 2005,
+   "category" => "Books",
+   "authors" => [
+       " Rick Riordan",
+
+   ],
+   "publisher" => "Miramax Books",
+   "isbn" => '9780399185045'
+];
+
+*//Movies*
+$catalog[201] = [
+   "title" => "8mile",
+   "img" => "media/img/Movie1.jpg",
+   "genre" => "Drama,Hip Hop Movie",
+   "format" => "DVD",
+   "year" => 2002,
+   "category" => "Movies",
+   "director" => "Curtis Hanson",
+   "writers" => [
+       "Scott Silver"
+
+   ],
+   "stars" => [
+       "Eminem ",
+       "Brittany Murphy",
+       "Kim Basinger"
+   ]
+];
+
+*//Music*
+$catalog[301] = [
+   "title" => "Marshell Mathers LP",
+   "img" => "media/img/Music4.jpg",
+   "genre" => "Hip Hop",
+   "format" => "CD",
+   "year" => 2000,
+   "category" => "Music",
+   "artist" => "Eminem"
+]; (edited)
+<?
---
+++ inc/footer.php
@@ -0,0 +1,12 @@
+<div class=“footer”>
+ <div class=“wrapper”>
+   <ul>
+     <li><a href=“https://www.instagram.com/LaGuardiaCC/“>twitter</a></li>
+     <li><a href=“https://www.facebook.com/LaGuardiaCC”>facebook</a></li>
+   </ul>
+   <p>&copy;<?php echo date(“Y”); ?>2018 My Online Library Collection</p>
+ </div>
+</div>
+
+</body>
+</html>
